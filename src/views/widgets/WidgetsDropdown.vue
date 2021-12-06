64096ccd8f2c54c8a87e803a89bfee1f4c37a126
Trung Nguyen <nguyenductrung532@gmail.com>
2021-12-06 23:30:11 +0700

    thongke

--- src/views/widgets/WidgetsDropdown.vue
+++ src/views/widgets/WidgetsDropdown.vue
@@ -1,16 +1,13 @@
 <template>
   <CRow>
     <CCol sm="6" lg="3">
-      <CWidgetDropdown color="primary" header="9.823" text="Members online">
+      <CWidgetDropdown color="primary" :header="countOrder" text="Tổng hóa đơn">
         <template #default>
-          <CDropdown
-            color="transparent p-0"
-            placement="bottom-end"
-          >
+          <CDropdown color="transparent p-0" placement="bottom-end">
             <template #toggler-content>
-              <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </template>
-            <CDropdownItem>Action</CDropdownItem>
+            <CDropdownItem>Action {{ countOrder }}</CDropdownItem>
             <CDropdownItem>Another action</CDropdownItem>
             <CDropdownItem>Something else here...</CDropdownItem>
             <CDropdownItem disabled>Disabled action</CDropdownItem>
@@ -20,7 +17,7 @@
           <CChartLineSimple
             pointed
             class="mt-3 mx-3"
-            style="height:70px"
+            style="height: 70px"
             :data-points="[65, 59, 84, 84, 51, 55, 40]"
             point-hover-background-color="primary"
             label="Members"
@@ -30,7 +27,11 @@
       </CWidgetDropdown>
     </CCol>
     <CCol sm="6" lg="3">
-      <CWidgetDropdown color="info" header="9.823" text="Members online">
+      <CWidgetDropdown
+        color="info"
+        :header="ComfimCountOrder"
+        text="Tổng số đơn hoàn thành"
+      >
         <template #default>
           <CDropdown
             color="transparent p-0"
@@ -38,7 +39,7 @@
             :caret="false"
           >
             <template #toggler-content>
-              <CIcon name="cil-location-pin"/>
+              <CIcon name="cil-location-pin" />
             </template>
             <CDropdownItem>Action</CDropdownItem>
             <CDropdownItem>Another action</CDropdownItem>
@@ -50,10 +51,10 @@
           <CChartLineSimple
             pointed
             class="mt-3 mx-3"
-            style="height:70px"
+            style="height: 70px"
             :data-points="[1, 18, 9, 17, 34, 22, 11]"
             point-hover-background-color="info"
-            :options="{ elements: { line: { tension: 0.00001 }}}"
+            :options="{ elements: { line: { tension: 0.00001 } } }"
             label="Members"
             labels="months"
           />
@@ -61,18 +62,11 @@
       </CWidgetDropdown>
     </CCol>
     <CCol sm="6" lg="3">
-      <CWidgetDropdown
-        color="warning"
-        header="9.823"
-        text="Members online"
-      >
+      <CWidgetDropdown color="warning" :header="CancerCountOrder" text="Số đơn bị hủy">
         <template #default>
-          <CDropdown
-            color="transparent p-0"
-            placement="bottom-end"
-          >
+          <CDropdown color="transparent p-0" placement="bottom-end">
             <template #toggler-content>
-              <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </template>
             <CDropdownItem>Action</CDropdownItem>
             <CDropdownItem>Another action</CDropdownItem>
@@ -83,10 +77,10 @@
         <template #footer>
           <CChartLineSimple
             class="mt-3"
-            style="height:70px"
+            style="height: 70px"
             background-color="rgba(255,255,255,.2)"
             :data-points="[78, 81, 80, 45, 34, 12, 40]"
-            :options="{ elements: { line: { borderWidth: 2.5 }}}"
+            :options="{ elements: { line: { borderWidth: 2.5 } } }"
             point-hover-background-color="warning"
             label="Members"
             labels="months"
@@ -95,18 +89,11 @@
       </CWidgetDropdown>
     </CCol>
     <CCol sm="6" lg="3">
-      <CWidgetDropdown
-        color="danger"
-        header="9.823"
-        text="Members online"
-      >
+      <CWidgetDropdown color="danger" :header="unconfirm.length" text="Số đơn chưa hoàn thành">
         <template #default>
-          <CDropdown
-            color="transparent p-0"
-            placement="bottom-end"
-          >
+          <CDropdown color="transparent p-0" placement="bottom-end">
             <template #toggler-content>
-             <CIcon name="cil-settings"/>
+              <CIcon name="cil-settings" />
             </template>
             <CDropdownItem>Action</CDropdownItem>
             <CDropdownItem>Another action</CDropdownItem>
@@ -117,7 +104,7 @@
         <template #footer>
           <CChartBarSimple
             class="mt-3 mx-3"
-            style="height:70px"
+            style="height: 70px"
             background-color="rgb(250, 152, 152)"
             label="Members"
             labels="months"
@@ -129,10 +116,140 @@
 </template>
 
 <script>
-import { CChartLineSimple, CChartBarSimple } from '../charts/index.js'
-
+import { CChartLineSimple, CChartBarSimple } from "../charts/index.js";
+import axios from "axios";
 export default {
-  name: 'WidgetsDropdown',
-  components: { CChartLineSimple, CChartBarSimple }
-}
+  name: "WidgetsDropdown",
+  components: { CChartLineSimple, CChartBarSimple },
+  data() {
+    return {
+      ComfimCountOrder: "",
+      countOrder: "",
+      CancerCountOrder:"",
+      unconfirm:"",
+    };
+  },
+  mounted() {
+    axios
+      .get("http://150.95.105.29:8800/api/admin/report/getComfimCountOrder", {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYW5oIiwiaWF0IjoxNjM4ODAzMDExLCJleHAiOjE2Mzg4ODk0MTF9.m5sAvi8Y66YQ_0GcaeMBChBukRUWKgcK-Nj4nKxh6gXkgKjVE9qYqjD-805vrKhpYnZWnK22fVQfYKmWIp3D6Q",
+        },
+      })
+      .then((response) => {
+        console.log(this);
+        this.ComfimCountOrder = response.data.object.toString();
+        console.log(this.ComfimCountOrder);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    axios
+      .get("http://150.95.105.29:8800/api/admin/report/getCountOrder", {
+        headers: {
+          Authorization: this.$store.state.userToken,
+        },
+      })
+      .then((response) => {
+        this.getData = response.data.object;
+        this.countOrder = response.data.object.toString();
+        console.log(this.countOrder);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+     axios
+      .get("http://150.95.105.29:8800/api/admin/report/getCancerCountOrder", {
+        headers: {
+          Authorization: this.$store.state.userToken,
+        },
+      })
+      .then((response) => {
+        this.getData = response.data.object;
+        this.CancerCountOrder = response.data.object.toString();
+        console.log(this.CancerCountOrder);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    axios
+      .get("http://150.95.105.29:8800/api/admin/report/unconfirm", {
+        headers: {
+          Authorization: this.$store.state.userToken,
+        },
+      })
+      .then((response) => {
+        this.getData = response.data.object;
+        this.unconfirm = response.data.object;
+        console.log(this.unconfirm);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+  methods: {
+    getCancerCountOrder() {
+      axios
+        .get(this.$store.state.MainLink + "admin/report/getCancerCountOrder", {
+          headers: {
+            Authorization: this.$store.state.userToken,
+          },
+        })
+        .then((response) => {
+          this.getData = response.data.object;
+          console.log(this.getData);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    getComfimCountOrder() {
+      axios
+        .get(this.$store.state.MainLink + "admin/report/getComfimCountOrder", {
+          headers: {
+            Authorization: this.$store.state.userToken,
+          },
+        })
+        .then((response) => {
+          this.getData = response.data.object;
+          console.log(this.getData);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    getCountOrder() {
+      axios
+        .get(this.$store.state.MainLink + "api/admin/report/getCountOrder", {
+          headers: {
+            Authorization: this.$store.state.userToken,
+          },
+        })
+        .then((response) => {
+          this.getData = response.data.object;
+          this.countOrder = response.data.object;
+          console.log(this.getData);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    getListUnConfirmOrders() {
+      axios
+        .get(this.$store.state.MainLink + "admin/report/unconfirm", {
+          headers: {
+            Authorization: this.$store.state.userToken,
+          },
+        })
+        .then((response) => {
+          this.getData = response.data.object;
+          console.log(this.getData);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
 </script>
